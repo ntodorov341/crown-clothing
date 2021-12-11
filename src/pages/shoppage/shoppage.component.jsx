ef58a7999a1c3dc8c85aa4e22031452fe54c804a
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-11 13:54:12 +0200

    updated the shoppage component to render collection preview instead of a simple div

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import CollectionPreview from '../../components/preview-collection/collection-preview.component.jsx';
 
-import SHOP_DATA from './shop.data.js';
+import { SHOP_DATA } from './shop.data.js';
 
 const ShopPage = () => {
     return (
-        <div>SHOP PAGE</div>
+        <div className='shop-page'>
+            {
+                SHOP_DATA.map(({id, ...collectionProps}) =>(
+                    <CollectionPreview key={id} {...collectionProps} />
+                ))
+            }
+        </div>
     )
 };
 

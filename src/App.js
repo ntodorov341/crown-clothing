decedb98a671c8c33352dd4a14eee3840843b818
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-11 12:40:39 +0200

    added react-router to the project and created routes for homepage and shoppage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }

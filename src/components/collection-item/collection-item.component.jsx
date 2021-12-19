1fb32fc24cb8b02ec23031350145e7564e1192ab
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-19 19:34:59 +0200

    added logic to the add to cart button for updating the cart state in redux with the selected item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
 const CollectionItem = (props) => {
-    const { name, imageUrl, price } = props;
+    const { product } = props;
+    const { imageUrl, name, price } = product;
+
+    const dispatch = useDispatch();
+
     return (
         <div className='collection-item'>
             <div
@@ -18,7 +24,11 @@ const CollectionItem = (props) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <CustomButton label='Add to cart' inverted />
+            <CustomButton
+                label='Add to cart'
+                inverted
+                onClick={() => dispatch(addItem(product))}
+            />
         </div>
     )
 };

f013cd449a123bd9193155b7f2d8146df0f79033
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-20 22:29:09 +0200

    styled the cart item component and added logic for rendering them in the cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+    const cartItems = useSelector((state) => state.cart.cartItems);
+
     return (
         <div className='cart-dropdown'>
             <div className='cart-items'>
-                <CustomButton label="GO TO CHECKOUT" />
+                {cartItems.map((cartItem) => <CartItem key={cartItems.id} item={cartItem} />)}
             </div>
+            <CustomButton label="GO TO CHECKOUT" />
         </div>
     )
 };

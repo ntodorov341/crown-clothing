e8eff397bef7e2c9ee3a893664e5a3438a38a9c9
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-20 22:30:09 +0200

    updated the item count in the cart icon to correspond to the total amount of products added to the cart instead of being hardcoded 0

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 import { ReactComponent as ShoppingCart } from '../../assets/shopping-bag.svg';
+import { useSelector } from 'react-redux';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-    <div className='cart-icon'>
-        <ShoppingCart className='shopping-icon' />
-        <span className='item-count'>0</span>
-    </div>
-)
+const CartIcon = () => {
+    const itemCount = useSelector(
+        (state) => state.cart.cartItems.reduce(
+            (accumulatedQuantity, item) => accumulatedQuantity + item.quantity, 0,
+        ),
+    );
+    return (
+        <div className='cart-icon'>
+            <ShoppingCart className='shopping-icon' />
+            <span className='item-count'>{itemCount}</span>
+        </div>
+    )
+};
 
 export default CartIcon;

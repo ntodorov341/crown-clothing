7280204f3ea18fe2f3136d11b9ebc4886701ca69
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-23 22:27:04 +0200

    added an utility function for decreasing the quantity for an item if it is more than 1

--- src/redux/cart/utils.js
+++ src/redux/cart/utils.js
@@ -15,4 +15,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     // executes the first time an item is added to the array and initializes the quantity with 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
-}
+};
+
+export const decreaseItemQuantity = (cartItems, cartItemToDecrease) => {
+    // check if the item already exists in the cart item array
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToDecrease.id,
+    );
+    
+    // if the item exists increase and the quantity is more than 1 decrease it by 1
+    if (existingCartItem && existingCartItem.quantity > 1) {
+        return cartItems.map(
+            (cartItem) => cartItem.id === cartItemToDecrease.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+        )
+    }
+
+    // return the cart items if the quantity is 1
+    return [...cartItems]
+};

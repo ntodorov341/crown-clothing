2a88f426cabb97d3cce19dce34be92917a52d98a
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-23 20:30:04 +0200

    added a conditional render to show a message if there are no items in the cart and logic to the go to checkout button to redirect the user to the checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
     const cartItems = useSelector((state) => selectCartItems(state));
 
     return (
         <div className='cart-dropdown'>
             <div className='cart-items'>
-                {cartItems.map((cartItem) => <CartItem key={cartItems.id} item={cartItem} />)}
+                {
+                    cartItems.length
+                    ? cartItems.map((cartItem) => <CartItem key={cartItems.id} item={cartItem} />)
+                    : <span className='empty-message'>There is nothing here yet &#128542;</span>
+                }
             </div>
-            <CustomButton label="GO TO CHECKOUT" />
+            <CustomButton
+                label="GO TO CHECKOUT"
+                onClick={() => {
+                    dispatch(toggleCartHidden());
+                    navigate('/checkout');
+                }}
+            />
         </div>
     )
 };

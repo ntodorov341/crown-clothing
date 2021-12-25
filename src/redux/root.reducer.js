861a8c2ab84f68e383ff631c5ec93e9effb7017c
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2021-12-25 20:22:36 +0200

    created a persisted store which allows our state to be cached by the browser, added root reducer persisted config and persisted the reducer as well, added an entry point (PersistGate) for persisting the store to the application

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+// local storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'], // contains the names of all the reducers that we want to persist
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+
+// exports the persisted version of the root reducer with the config applied
+export default persistReducer(persistConfig, rootReducer);

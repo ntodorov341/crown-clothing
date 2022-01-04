9afcae3a7f34125180058c35a6cb24333913df5f
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2022-01-04 23:40:43 +0200

    added directory reducer and selector moving our sections state to redux in order to easen up the testing

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -6,6 +6,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
     key: 'root',
@@ -16,6 +17,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
+    directory: directoryReducer,
 });
 
 // exports the persisted version of the root reducer with the config applied

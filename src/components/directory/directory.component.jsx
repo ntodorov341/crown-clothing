921e0ec7381956c3e1b515acb51ed1070759ec04
Nikolay Todorov <nikolaytodorov3411@gmail.com>
2022-01-04 23:41:18 +0200

    updated the directory component to take the sections from redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,42 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from "../menu-item/menu-item.component";
 
 import './directory.styles.scss';
 
-const data = [
-    {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      linkUrl: 'shop/hats'
-    },
-    {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      linkUrl: 'shop/jackets'
-    },
-    {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      linkUrl: 'shop/sneakers'
-    },
-    {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
-      linkUrl: 'shop/womens'
-    },
-    {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      size: 'large',
-      linkUrl: 'shop/mens'
-    }
-];
-
-const Directory = () => (
+const Directory = () => {
+  const sections = useSelector((state) => selectDirectorySections(state));
+  return (
     <div className="directory-menu">
         {
-            data.map(({ title, imageUrl, size }) => (
+          sections.map(({ title, imageUrl, size }) => (
                 <MenuItem
                     key={imageUrl}
                     title={title.toUpperCase()}
@@ -46,6 +22,7 @@ const Directory = () => (
             ))
         }
     </div>
-)
+  )
+};
 
 export default Directory;
